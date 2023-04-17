5f3c89aeaf392640a56b629148c37a2fad23361f
SUNG_JUN <lalalalalalala0430@gmail.com>
2023-04-17 09:07:49 +0900

    슬라이드 뷰 수정

--- Web_PRO/main/js/slide_banner.js
+++ Web_PRO/main/js/slide_banner.js
@@ -1,20 +1,19 @@
 const outer = document.querySelector('#outer');
 const innerList = document.querySelector('#inner-list');
-
 const inners = document.querySelectorAll('.inner');
-
+//html에서의 DOM 객체들 id와 class를 통해 불러옴
 let currentIndex = 0;
-
+//이미지의 인덱스 표시
 
 inners.forEach((inner) => {
     inner.style.width = `${outer.clientWidth}px`;
 })
-
+//각각의 사진 요소들 width값 설정 해주고 출력
 innerList.style.width = `${outer.clientWidth * inners.length}px`;
 
 const buttonLeft = document.querySelector('#button-left');
 const buttonRight = document.querySelector('#button-right');
-
+//버튼 DOM 가져옴
 buttonLeft.addEventListener('click', () => {
     currentIndex--;
     currentIndex = currentIndex < 0 ? 0 : currentIndex;
@@ -23,7 +22,9 @@ buttonLeft.addEventListener('click', () => {
 
     clearInterval(interval);
     interval = getInterval();
+    //자연스럽게 화면을 넘어가게 해줌
 });
+//좌측 버튼 클릭 했을 경우 인덱스 -1 하고 만약 0을 보다 작으면 0으로 초기화
 
 buttonRight.addEventListener('click', () => {
     currentIndex++;
@@ -35,7 +36,7 @@ buttonRight.addEventListener('click', () => {
 
     interval = getInterval();
 });
-
+//우측 버튼 클릭 했을 경우 인덱스 +1 하고 만약 사진의 최대 개수를 넘었다면 다시 그 개수로 초기화
 const getInterval = () => {
     return setInterval(() => {
         currentIndex++;
@@ -44,5 +45,5 @@ const getInterval = () => {
 
     }, 8000);
 }
-
+//8초 동안 보여주고 자연 스럽게 화면 넘어가게 해줌 (최대 개수를 넘었다면 0으로 처음으로 돌아가게 함)
 let interval = getInterval();
